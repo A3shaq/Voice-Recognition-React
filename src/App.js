1cdff4b8d80a6f4b804324f92432c24bbc3376d4
Muhammad Arshaq <arshaqshakeel3@gmail.com>
2021-03-13 02:33:00 +0500

    feat(React Speech Recognition): * Test / integrate react speech recognition library

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+
+import React, { useState, useEffect } from "react"
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition'
+
 import './App.css';
 
+// const speechRecognition = window.speechRecognition || window.webkitSpeechRecognition;
+// const recognition = new SpeechRecognition()
+
+// recognition.start()
 function App() {
+  const [counter, setCounter] = useState(0)
+  const { transcript, resetTranscript } = useSpeechRecognition()
+
+  if (!SpeechRecognition.browserSupportsSpeechRecognition()) {
+    return null
+  }
+
+  // const voiceCommands = ()=>{
+  //   recognition.onstart =()=>{
+  //     console.log('voiceCommands')
+  //   } 
+  // }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      {/* 
+      <h1>Counter: {counter}</h1>
+
+      <button onClick={() => setCounter(counter + 1)}>+</button>
+      <button onClick={() => counter > 0 && setCounter(counter - 1)}>-</button> */}
+
+      <button onClick={SpeechRecognition.startListening}>Start</button>
+      <button onClick={SpeechRecognition.stopListening}>Stop</button>
+      <button onClick={resetTranscript}>Reset</button>
+      <h1>{transcript}</h1>
     </div>
   );
 }
